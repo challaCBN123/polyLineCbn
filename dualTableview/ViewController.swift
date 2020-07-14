731e2e74ee0c2825f0724f693266318a9595f113
challaCBN123 <challabhargava55@gmail.com>
2020-07-14 20:58:59 +0530

    Initial Commit

---
+++ dualTableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dualTableview
+//
+//  Created by Bhargava on 14/07/20.
+//  Copyright © 2020 Bhargava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
